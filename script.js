bbe19faab3a3b7edfbded9cb919e28b8917acd12
rafarriza <rizarey93@gmail.com>
2024-10-31 11:01:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website loaded successfully!");
-});
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => section.classList.remove('active'));
+    
+    document.getElementById(sectionId).classList.add('active');
+}
